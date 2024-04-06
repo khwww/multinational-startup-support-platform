822fb2f677b0f31be5794174e34c6f467eac0010
mini-777 <pho0570@gmail.com>
2024-04-06 16:24:52 +0900

    블로그 게시물 제목 및 이미지 변경

--- client/_posts/preview.md
+++ client/_posts/preview.md
@@ -1,10 +1,10 @@
 ---
-title: '게시글 제목'
-excerpt: '인용자'
+title: '2024년 글로벌 기업 협업 프로그램 창업기업 모집'
+excerpt: '기업 협업 프로그램'
 coverImage: '/assets/blog/preview/cover.jpg'
 date: '2020-03-16T05:35:07.322Z'
 author:
-  name: 작성자
+  name: K-Start Up
   picture: '/assets/blog/authors/joe.jpeg'
 ogImage:
   url: '/assets/blog/preview/cover.jpg'
--- client/src/app/_components/cover-image.tsx
+++ client/src/app/_components/cover-image.tsx
@@ -1,6 +1,6 @@
-import cn from "classnames";
-import Link from "next/link";
-import Image from "next/image";
+import cn from 'classnames';
+import Link from 'next/link';
+import Image from 'next/image';
 
 type Props = {
   title: string;
@@ -13,17 +13,17 @@ const CoverImage = ({ title, src, slug }: Props) => {
     <Image
       src={src}
       alt={`Cover Image for ${title}`}
-      className={cn("shadow-sm w-full", {
-        "hover:shadow-lg transition-shadow duration-200": slug,
+      className={cn('shadow-sm w-full', {
+        'hover:shadow-lg transition-shadow duration-200': slug,
       })}
-      width={1300}
+      width={1200}
       height={630}
     />
   );
   return (
-    <div className="sm:mx-0">
+    <div className='sm:mx-0'>
       {slug ? (
-        <Link as={`/posts/${slug}`} href="/posts/[slug]" aria-label={title}>
+        <Link as={`/posts/${slug}`} href='/posts/[slug]' aria-label={title}>
           {image}
         </Link>
       ) : (
